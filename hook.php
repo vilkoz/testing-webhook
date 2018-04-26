c2795dc8c797319513e18eb23ed91aa67d3db808
vilkoz <vilko.a@gmail.com>
2018-04-26 13:01:45 +0300

    add git update on webhook

--- hook.php
+++ hook.php
@@ -4,6 +4,7 @@ print_r($_SERVER);
 print_r($_POST);
 
 file_put_contents("GITHUB_WEBHOOK_LAST_NON_VERIFIED", "here post" . serialize($_POST));
+shell_exec("git pull");
 if( 'sha1=' . hash_hmac( 'sha1', $postBody, $_ENV['GITHUB_SECRET'], false ) === $_SERVER[ 'HTTP_X_HUB_SIGNATURE' ])
 {
 	echo "super secret data!";
